670714c8ac1b5ee425e7af1474101f396605c6b6
Craig_Donavin <crdonavin@gmail.com>
2019-12-17 19:13:51 -0600

    tickets controller framework

---
+++ new-ticket-api/app/controllers/api/v1/ticket_controller.rb
@@ -0,0 +1,40 @@
+class TicketController < ApplicationController
+    def index
+        @tickets = Ticket.all
+
+        render json: @tickets, status: 200
+    end
+
+    def show
+        @ticket = Ticket.find(params[:id])
+
+        render json: @ticket, status: 200
+    end
+
+    def create
+        @ticket = Ticket.create(ticket_params)
+
+        render json: @ticket, status: 200
+    end
+
+    def update
+        @ticket = Ticket.find(params[:id])
+        @ticket.update(ticket_params)
+
+        render json: @ticket, status: 200
+    end
+
+    def destroy
+        @ticket = Ticket.find(params[:id])
+        @ticket.delete
+        
+        render json: {ticketId: @ticket.id}
+    end
+
+    private
+
+    def ticket_params
+    end
+
+
+end
