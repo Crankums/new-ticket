84d8878c2d63bdbea962a4dac0eb8b4a60e35757
Craig_Donavin <crdonavin@gmail.com>
2019-12-19 20:51:26 -0600

    creates form elements

--- js-frontend/src/components/form.js
+++ js-frontend/src/components/form.js
@@ -2,4 +2,26 @@ class Form {
     constructor() {
         
     }
+
+    renderInputs() {
+        let formContainer = document.getElementById('new-ticket-form')
+// 2 ways to do this: 
+// 1. Create each element individually and set their attributes
+// 2. Create an iterator 
+        for (let key in object) {
+            let input =  document.createElement('input')
+            input.id = `${key}`
+            formContainer.appendChild('input')
+        }   
+
+    }
+
+    renderForm() {
+        this.renderInputs()
+        let submit = document.createElement('submit')
+        submit.setAttribute('value', 'Submit Ticket')
+        formContainer.appendChild('submit')
+    }
 }
+
+
