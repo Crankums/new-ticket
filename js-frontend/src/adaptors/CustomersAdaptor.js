660151d814599013e983d978b82e67ab27bd2976
Craig_Donavin <crdonavin@gmail.com>
2019-12-19 20:59:33 -0600

    bundle update

---
+++ js-frontend/src/adaptors/CustomersAdaptor.js
@@ -0,0 +1,10 @@
+class TicketsAdapter {
+    constructor() {
+        this.baseUrl = 'http:localhost:3000/api/v1/customers'
+    }
+
+    getTickets() {
+        return fetch(this.baseUrl)
+        .then(res => res.json())
+    }
+}
