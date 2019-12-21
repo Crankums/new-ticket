b1cf89ef9cafa24720c63c3e67716b399ae5ee00
Craig_Donavin <crdonavin@gmail.com>
2019-12-20 21:58:54 -0600

    pseudo for find customer, fetchBikes, and bikeDropDown

--- js-frontend/src/components/tickets.js
+++ js-frontend/src/components/tickets.js
@@ -10,6 +10,31 @@ class Tickets {
         e.preventDefault()
     }
 
+    /*if findCustomer => getCustomer(){
+        return customer.name === inputValue.name
+    } else {
+        createCustomer(inputValues)  {
+            return customer
+        }
+    } <--this then sets who the customer is. we can do something similar re the bike */
+
+    /*  fetchCustomerBikes(){
+            fetch() {return bikes array or undefined}
+            
+    }
+        createBikeDropdown(bikeArray){
+            let dropdown = document.createElement("dropdown")
+            iterate bikeArray to populate dropdown 
+            append just below contact block
+        }
+    
+    renderForm() {
+        
+    }
+
+
+
+
     /* render completed ticket in its own div, justified right. each list item is given its own checkbox.
     div id: rendered-ticket
         div contact-block
