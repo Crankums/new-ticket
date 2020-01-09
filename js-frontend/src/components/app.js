21be9111a6273b3811ddf09a9892a6b8b3d3123d
Craig_Donavin <crdonavin@gmail.com>
2020-01-09 13:12:14 -0600

    add constructor, event listeners, and handle submit to app.js

--- js-frontend/src/components/app.js
+++ js-frontend/src/components/app.js
@@ -1,5 +1,21 @@
 class App {
     constructor() {
-        this.tickets = new Tickets()
+        this.customerContainer = document.querySelector('customer-container')
+        this.ticketContainer = document.querySelector('ticket-container')
+        this.customerAdapter = new CustomerAdapter
+        this.currentCustomer
+        initBindingsAndEventListeners()
+    }
+
+    initBindingsAndEventListeners(){
+        this.form = document.querySelector('customer-form')
+        this.form.addEventListener('submit', this.handleCustomerSubmit.bind(this))
+    }
+
+    handleCustomerSubmit(e) {
+        e.preventDefault()
+        const customerName = this.form.children[0].value
+        const customerEmail = this.form.children[1].value
+
     }
 }
