49ec4e82d07d8819e2922e31ce92a720b5f67d1f
Craig_Donavin <crdonavin@gmail.com>
2020-01-11 19:26:59 -0600

    fetch methods for ticketsAdapter

--- js-frontend/src/adaptors/TicketsAdapter.js
+++ js-frontend/src/adaptors/TicketsAdapter.js
@@ -6,6 +6,9 @@ class TicketsAdapter {
     fetchTickets() {
         return fetch(this.baseUrl, {
             method: "GET",
+            headers: {
+                'Content-Type':'application/json'
+            },
             body: JSON.stringify(params)
         })
         .then(res => res.json())
@@ -14,7 +17,22 @@ class TicketsAdapter {
     createTickets() {
         fetch(this.baseUrl, {
             method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify(params)
         })
     }
+
+    updateTickets() {
+        fetch(this.baseUrl, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        })
+    }
+
+    // is an update method necessary? if ticket is already created, otherwise updates and adds a line
 }
