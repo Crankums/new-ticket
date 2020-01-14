489c29d1d48d67c7e65000b8461ef638b75debd3
Craig_Donavin <crdonavin@gmail.com>
2020-01-13 20:18:24 -0600

    controller stuff

--- js-frontend/src/adaptors/CustomersAdapter.js
+++ js-frontend/src/adaptors/CustomersAdapter.js
@@ -2,9 +2,17 @@ class CustomersAdapter {
     constructor() {
         this.baseUrl = 'http:localhost:3000/api/v1/customers'
     }
+    
+    
 
     fetchCustomer(params){
-        fetch(this.baseUrl)
+        fetch(this.baseUrl, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+        })
         .then(res => res.json())
     }
     
