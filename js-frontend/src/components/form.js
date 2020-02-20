a013f8ee810e7ff880be2e7c81ae2d8c768994cc
Craig_Donavin <crdonavin@gmail.com>
2020-02-20 12:52:46 -0600

    removes form adapter

--- js-frontend/src/components/form.js
+++ js-frontend/src/components/form.js
@@ -2,7 +2,7 @@ class Form {
     constructor() {
         this.currentTicket = false
         this.ticketAdapter = new TicketsAdapter
-        this.formsAdapter = new FormsAdapter
+        // this.formsAdapter = new FormsAdapter
     }
 
 
